d675430fc5f64822113d67e2e72897906e6a9483
Karreg <krystanos@gmail.com>
2016-01-31 12:43:37 +0100

    - Add project for model and database (using LiteDB)

---
+++ surtr.Model/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace surtr.Model
+{
+    using System.Drawing;
+
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public Image Cover { get; set; }
+    }
+}
