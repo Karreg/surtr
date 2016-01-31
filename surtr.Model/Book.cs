f7c1c8e3202ab77b387249f298204edb9727de5b
Karreg <krystanos@gmail.com>
2016-01-31 13:02:38 +0100

    - Model definition continued

--- surtr.Model/Book.cs
+++ surtr.Model/Book.cs
@@ -1,17 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace surtr.Model
+﻿namespace surtr.Model
 {
+    using System;
     using System.Drawing;
+    using System.IO;
 
     public class Book
     {
+        /// <summary>
+        /// Title of the book
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        /// Cover of the book
+        /// </summary>
         public Image Cover { get; set; }
+
+        /// <summary>
+        /// Date the file was added to database
+        /// </summary>
+        public DateTime AddDate { get; set; }
+
+        /// <summary>
+        /// Modification date of the file
+        /// </summary>
+        public DateTime ModificationDate { get; set; }
+
+        /// <summary>
+        /// Creation date of the file
+        /// </summary>
+        public DateTime CreationDate { get; set; }
+
+        /// <summary>
+        /// Filename
+        /// </summary>
+        public string Filename { get; set; }
+
+        /// <summary>
+        /// Filepath (without filename)
+        /// </summary>
+        public string Filepath { get; set; }
+
+        /// <summary>
+        /// Full file path
+        /// </summary>
+        public string FullFilePath => Path.Combine(this.Filepath, this.Filename);
+
+        /// <summary>
+        /// ID, using the full file path
+        /// </summary>
+        public string Id => this.FullFilePath;
+
+        /// <summary>
+        /// Size of the file in bytes
+        /// </summary>
+        public double FileSize { get; set; }
     }
 }
---
+++ surtr.Model/BookSet.cs
@@ -0,0 +1,17 @@
+﻿namespace surtr.Model
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Definition of a bookset (full library or playlist)
+    /// </summary>
+    public class BookSet : Dictionary<string, Book>
+    {
+        public string Name { get; set; }
+
+        public void Add(Book book)
+        {
+            base.Add(book.Id, book);
+        }
+    }
+}
